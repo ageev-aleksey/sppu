7e33418ad1651d2d4866391c431f392827c6b30c
nrx <ageevav97@gmail.com>
2021-01-19 11:00:32 +0300

    начат виджет для взаимодействия через comport

--- include/gui/QSddDeviceControl.h
+++ include/gui/QSddDeviceControl.h
@@ -5,7 +5,7 @@
 #ifndef SDDCLIENT_QSDDDEVICECONTROL_H
 #define SDDCLIENT_QSDDDEVICECONTROL_H
 #include "gui/QISddStateWidget.h"
-#include "sdd_protocol/connect/IConnection.h"
+#include "sdd_protocol/connect/QSerialPortConnection.h"
 #include <QtWidgets>
 
 #include <memory>
@@ -13,11 +13,37 @@
 
 class QSddDeviceControl : public QISddStateWidget {
 public:
-    explicit QSddDeviceControl(std::unique_ptr<sdd::conn::IConnection> connection);
+    explicit QSddDeviceControl(std::unique_ptr<QSerialPort> SerialPort);
     std::vector<sdd::conn::State> getSddStates() override;
 private:
     void guiInit();
-    std::unique_ptr<sdd::conn::IConnection> m_pConnection;
+
+    std::unique_ptr<sdd::conn::QSerialPortConnection> m_pSender;
+    std::shared_ptr<QSerialPort> m_pSerialPort;
+
+    QVBoxLayout *mLayout = new QVBoxLayout;
+
+    // Вывод текущего состояние устройства
+    QLabel *mXPosition = new QLabel;
+    QLabel *mYPosition = new QLabel;
+    QLabel *mXSpeed = new QLabel;
+    QLabel *mYSpeed = new QLabel;
+
+    // Вкладка управления устройством
+    QWidget *mControlWidget = new QWidget; /// Корневой виджет вкладки управления
+    // 1. Выдача задания на управление
+    QSpinBox *mOxTask = new QSpinBox;
+    QSpinBox *mOyTask = new QSpinBox;
+    // 2. Управление светодиодом
+    QDoubleSpinBox *mLightBlinking = new QDoubleSpinBox;
+    // 3. Установка скважности ШИМ
+    QSpinBox *mPwmOx = new QSpinBox;
+    QSpinBox *mPwmOy = new QSpinBox;
+    QCheckBox *mModePwmControl = new QCheckBox;
+    // Вкладка настройки КомПорта
+    // 1. Скорость передачи битэ
+    // 2. ...
+
 };
 
 #endif //SDDCLIENT_QSDDDEVICECONTROL_H
