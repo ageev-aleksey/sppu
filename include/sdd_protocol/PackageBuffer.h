4379a4940f7b3e144fa9bd7b80cc60cd81c023f9
Агеев Алексей Владимирович <ageevav97@gmail.com>
2021-01-28 16:31:52 +0300

    Ошибка, не находит заголовок boost

--- include/sdd_protocol/PackageBuffer.h
+++ include/sdd_protocol/PackageBuffer.h
@@ -2,30 +2,30 @@
 //// Created by Aleksey on 15.03.2020.
 ////
 
-//#ifndef SPPU_SERIAL_PACKAGEBUFFER_H
-//#define SPPU_SERIAL_PACKAGEBUFFER_H
+#ifndef SPPU_SERIAL_PACKAGEBUFFER_H
+#define SPPU_SERIAL_PACKAGEBUFFER_H
 
-//#include "StatePackage.h"
-//#include <boost/circular_buffer.hpp>
-//#include <ostream>
+#include "StatePackage.h"
+#include <>///?
+#include <ostream>
 
-//namespace sdd {
-//    class PackageBuffer {
-//    public:
-//        explicit PackageBuffer(size_t size);
-//        void addBytes(const std::vector<unsigned char> &bytes);
-//        StatePackage formPackage();
-//        void flush();
-//        friend std::ostream &operator<<(std::ostream &stream, const PackageBuffer &buff);
-////private:
-//        boost::circular_buffer<unsigned char> buffer;
-//    };
-//}
+namespace sdd {
+    class PackageBuffer {
+    public:
+        explicit PackageBuffer(size_t size);
+        void addBytes(const std::vector<unsigned char> &bytes);
+        StatePackage formPackage();
+        void flush();
+        friend std::ostream &operator<<(std::ostream &stream, const PackageBuffer &buff);
+private:
+        boost::circular_buffer<unsigned char> buffer;
+    };
+}
 ////TODO ����� ���������� ������� ��� ������ ���� ������� - StatePackage. /
 //// ���������� ����������, ����� ����� ������� ��� ����� �������. /
 //// ��� ����� ���������� ����������� ����� �������
 
 
-//std::ostream &operator<<(std::ostream &stream, const sdd::PackageBuffer &buff);
+std::ostream &operator<<(std::ostream &stream, const sdd::PackageBuffer &buff);
 
-//#endif //SPPU_SERIAL_PACKAGEBUFFER_H
+#endif //SPPU_SERIAL_PACKAGEBUFFER_H
